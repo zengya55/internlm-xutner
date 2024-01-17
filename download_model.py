852efd354906854a578a63dd71ce0e9ffa323eb5
zengya55 <68380292+zengya55@users.noreply.github.com>
2024-01-17 19:33:41 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -5,4 +5,3 @@ import os
 model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/home/xlab-app-center/model', revision='v1.0.3')
 download(model_repo='zengya55/internlm-xtuner-zengya55', output='/home/xlab-app-center/model/hf')
 
-os.system('xtuner convert merge /home/xlab-app-center/model/Shanghai_AI_Laboratory/internlm-chat-7b /home/xlab-app-center/model/hf /home/xlab-app-center/model/merged --max-shard-size 2GB') 
